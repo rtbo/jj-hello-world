c92d7ef93de415e5894f26544f4be5f81fd785d4
Rémi Thebault <remi.thebault@gmail.com>
2025-06-30 00:18:35 +0200

    create hello and bye funcs

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// I shit on tutos...
 
 fn main() {
+    print_hello();
+    print_bye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_bye() {
+    println!("Bye, now...");
 }
